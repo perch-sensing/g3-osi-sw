7e81ab406fc34af8af36a08e843315a548b37ddc
Diego Andrade <bets636@gmail.com>
2022-04-13 16:21:24 -0700

    Making tflite_test script more path robust

--- test/ImageClassification/tflite_test.sh
+++ test/ImageClassification/tflite_test.sh
@@ -1,5 +1,4 @@
-BASE_DIR=~/g3-osi-sw
-TEST_DIR=${BASE_DIR}/test
+TEST_DIR="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 
 PHOTO_FILE=${TEST_DIR}/tmp/grace_hopper.bmp
 MODEL_FILE=${TEST_DIR}/tmp/mobilenet_v1_1.0_224.tflite
@@ -19,7 +18,6 @@ fi
 
 # Set up model mobilenet_v1__1.0_224
 if ! test -f "$MODEL_FILE"; then
-    # Get model
     echo "\nDownloading tflite model..."
     curl https://storage.googleapis.com/download.tensorflow.org/models/mobilenet_v1_2018_02_22/mobilenet_v1_1.0_224.tgz | tar xzv -C ${TEST_DIR}/tmp
 fi
